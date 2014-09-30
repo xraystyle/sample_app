17449bf9f63445875f12cbd9011d87af79e7fccc
xraystyle <xraystyle@xraystyle.com>
2014-09-30 13:34:38 -0700

    Remove application layout, write tests for static_pages titles.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,13 +9,19 @@ describe "Static pages" do
 	# Describe Home Page -------------------------------------------------
 	
 	describe "Home page" do
+
 		
 		it "should have the content 'Sample App'" do
-
 			visit '/static_pages/home'
 			expect(page).to have_content('Sample App')
-
 		end
+
+
+		it "should have the title 'Home'" do
+			visit 'static_pages/home'
+			expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+		end
+
 
 	end
 
@@ -27,10 +33,18 @@ describe "Static pages" do
 
 	describe "Help page" do
 
+
 		it "should have the content 'Help'" do
 			visit '/static_pages/help'
 			expect(page).to have_content('Help')
 		end
+
+
+		it "should have the title 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+		end
+
 		
 	end
 
@@ -41,12 +55,18 @@ describe "Static pages" do
 
 	describe "About page" do
 
-		it "should have the content 'About Us'" do
 
+		it "should have the content 'About Us'" do
 			visit '/static_pages/about'
 			expect(page).to have_content('About Us')
-
 		end
+
+
+		it "should have the title 'About Us'" do
+			visit '/static_pages/about'
+			expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
+		end
+
 		
 	end
 
