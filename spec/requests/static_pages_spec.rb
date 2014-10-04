87465590e898e3d1e71c4b372c5ea346edca6a59
xraystyle <xraystyle@xraystyle.com>
2014-10-03 19:54:51 -0700

    Further refactor/DRY rspec tests, add more coverage for links on layout.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,13 +6,27 @@ describe "Static pages" do
 
 	subject { page }
 
+	# Shared tests for all static pages ----------------------------------
+	shared_examples_for "all static pages" do
+	
+		it { should have_selector('h1', text: heading) }
+		it { should have_title(full_title(page_title)) }
+
+	end
+	# End shared tests for all static pages -------------------------------
+
+
+
+
 	# Describe Home Page -------------------------------------------------
 	
 	describe "Home page" do
 		before { visit root_path }
 
-		it { should have_selector('h1', text: 'Sample App') }
-		it { should have_title(full_title('')) }
+		let(:heading)		{ 'Sample App' }
+		let(:page_title)	{ '' }
+
+		it_should_behave_like "all static pages"
 		it { should_not have_title('| Home') }
 
 	end
@@ -25,8 +39,10 @@ describe "Static pages" do
 	describe "Help page" do
 		before { visit help_path }
 
-		it { should have_selector('h1', text: 'Help') }
-		it { should have_title(full_title('Help')) }
+		let(:heading)		{ 'Help' }
+		let(:page_title)	{ 'Help' }
+
+		it_should_behave_like "all static pages"
 		
 	end
 
@@ -38,8 +54,10 @@ describe "Static pages" do
 	describe "About page" do
 		before { visit about_path }
 
-		it { should have_selector('h1', text: 'About Us') }
-		it { should have_title(full_title('About Us')) }
+		let(:heading)		{ 'About Us' }
+		let(:page_title)	{ 'About Us' }
+
+		it_should_behave_like "all static pages"
 		
 	end
 
@@ -54,13 +72,41 @@ describe "Static pages" do
 	describe "Contact Page" do
 		before { visit contact_path }
 
-		it { should have_selector('h1', text: 'Contact') }
-		it { should have_title(full_title('Contact Us')) }
+		let(:heading)		{ 'Contact' }
+		let(:page_title)	{ 'Contact Us' }
+
+		it_should_behave_like "all static pages"
 		
 	end
 
 
-	# End Contact About Page ---------------------------------------------
+	# End Contact Page -----------------------------------------------------
+
+
+	# Describe Correct Links on Layout --------------------------------------
+
+	it "should have the right links on the layout" do
+		visit root_path
+
+		click_link "About"
+		expect(page).to have_title(full_title('About Us'))
+		click_link "Help"
+		expect(page).to have_title(full_title('Help'))
+		click_link "Contact"
+		expect(page).to have_title(full_title('Contact Us'))
+		click_link "Home"
+		click_link "Sign Up Now!"
+		expect(page).to have_title(full_title('Sign Up'))
+		click_link "sample app"
+		expect(page).to have_title(full_title(''))
+
+
+	end
+
+
+
+
+	# End describe Correct Links on Layout -----------------------------------
 
 
 
