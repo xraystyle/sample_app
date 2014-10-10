6a3ce98f9fd24fec38f3486ec7aa7bb0a38259bf
xraystyle <xraystyle@xraystyle.com>
2014-10-09 18:35:51 -0700

    User model tests, create users table index migration.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,24 @@
 class User < ActiveRecord::Base
+
+	# validates hash options:
+	# validates :attribute,
+	#     			:presence => boolean,
+	#     			:numericality => boolean,
+	#     			:length => {:within => 0..10},
+	#     			:format => regex,
+	#     			:inclusion => {:in => [array_or_range]},
+	#     			:exclusion => {:in => [array_or_range]},
+	#     			:acceptance => boolean,
+	#     			:uniqueness => boolean,
+	#     			:confirmation => boolean
+
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+	validates :name, presence: true, length:  { maximum: 50 }
+
+	validates :email, presence: true, format: VALID_EMAIL_REGEX, uniqueness: { case_sensitive: false }
+
+
+
 end
