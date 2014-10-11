617c4bf6a7c2262117933ec93fcfc0c423ee8587
xraystyle <xraystyle@xraystyle.com>
2014-10-10 17:53:27 -0700

    Finish User model with secure_passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,12 +12,17 @@ class User < ActiveRecord::Base
 	#     			:uniqueness => boolean,
 	#     			:confirmation => boolean
 
-
+	# Validations
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 	validates :name, presence: true, length:  { maximum: 50 }
 
 	validates :email, presence: true, format: VALID_EMAIL_REGEX, uniqueness: { case_sensitive: false }
+
+	has_secure_password
+	validates :password, length: { minimum: 6 }
+
+	
 
 
 
