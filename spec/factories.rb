04b1d821bfe5f2201a66b480bbd3a5f41bede1d0
xraystyle <xraystyle@xraystyle.com>
2014-10-27 16:31:25 -0700

    add pagination, refactor to add user partial.

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,10 +3,12 @@
 FactoryGirl.define do 
 
 	factory :user do 
-		name "Michael Hartl"
-		email "michael@example.com"
+
+		sequence(:name) { |n| "Person #{n}" }
+		sequence(:email) { |n| "person_#{n}@example.com" }
 		password "foobar"
 		password_confirmation "foobar"
+
 	end
 	
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -20,11 +20,22 @@ describe "UserPages" do
 		it { should have_title('All Users') }
 		it { should have_content('All Users') }
 
-		it 'should list each user' do
-			User.all.each do |user|
-				expect(page).to have_selector('li', text: user.name)
+
+		describe 'pagination' do
+			before(:all) { 30.times { FactoryGirl.create(:user) } }
+			after(:all) { User.delete_all }
+
+			it { should have_selector('div.pagination') }
+	
+			it 'should list each user' do
+				User.paginate(page: 1).each do |user|
+					expect(page).to have_selector('li', text: user.name)
+				end
 			end
+
 		end
+
+
 
 
 
