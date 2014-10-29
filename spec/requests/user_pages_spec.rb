2c0c9fe0851c61334ab652fa5ebf4ad06eb2f9ef
xraystyle <xraystyle@xraystyle.com>
2014-10-29 14:45:38 -0700

    Ex. 1 and 2 complete.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -197,6 +197,19 @@ describe "UserPages" do
 
 		end
 
+		describe 'forbidden attributes' do
+			let(:params) do
+				{ user: { admin: true, password: user.password, password_confirmation: user.password } }
+			end
+
+			before do
+				sign_in(user, no_capybara: true)
+				patch user_path(user), params
+			end
+			specify { expect(user.reload).not_to be_admin }
+			
+		end
+
 	end
 
 
