c729017fd2cee934d5f38ab959e5e201fcdf2fc0
xraystyle <xraystyle@xraystyle.com>
2014-10-31 17:56:28 -0700

    Add index to microposts table, write basic tests, migrate db and prepare test db

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Micropost do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	let(:user) { FactoryGirl.create(:user) }
+	before do
+		# This code is not idiomatically correct.
+		@micropost = Micropost.new(content: "Lorem Ipsum", user_id: user.id)
+	end
+
+	subject { @micropost }
+
+	it { should respond_to(:content) }
+	it { should respond_to(:user_id) }
+
+
+
+
+
+
+
+
 end
