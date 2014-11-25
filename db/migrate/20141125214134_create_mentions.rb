baa446ff92ba1b8698d1aa9293ac587ed0e8c62e
xraystyle <xraystyle@xraystyle.com>
2014-11-25 13:42:07 -0800

    Generate mention model and associated files.

---
+++ db/migrate/20141125214134_create_mentions.rb
@@ -0,0 +1,10 @@
+class CreateMentions < ActiveRecord::Migration
+  def change
+    create_table :mentions do |t|
+      t.integer :micropost_id
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
